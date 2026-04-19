f8faeb0d4ef11e5d0d8920211393e2194f167abe
trevolpulse-svg <trevolpulse@gmail.com>
2026-04-19 18:28:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,4 @@
 import os
-from aiogram import Bot, Dispatcher, executor, types
 
-TOKEN = os.getenv("TOKEN")
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands=["start"])
-async def start(message: types.Message):
-    await message.answer("🤖 Бот запущен и работает!")
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(f"Ты написал: {message.text}")
-
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+print("БОТ ЗАПУСТИЛСЯ")
+print("TOKEN есть:", bool(os.getenv("TOKEN")))
