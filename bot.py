a98bf0c96ee78eca81ce316072e07f208a8cffef
trevolpulse-svg <trevolpulse@gmail.com>
2026-04-19 18:21:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,18 @@
-import asyncio
 import os
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, executor, types
 
 TOKEN = os.getenv("TOKEN")
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
-start_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="✈️ Найти тур")],
-        [KeyboardButton(text="🏨 Найти отель")]
-    ],
-    resize_keyboard=True
-)
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.answer("🤖 Бот запущен и работает!")
 
-@dp.message(F.text == "/start")
-async def start(message: Message):
-    await message.answer(
-        "Привет 👋\nЯ помогу найти дешёвые туры 🔥",
-        reply_markup=start_kb
-    )
-
-@dp.message(F.text == "✈️ Найти тур")
-async def tour(message: Message):
-    await message.answer("Вот лучшие туры 👇\nhttps://level.tpx.lv/shWiTpL2")
-
-@dp.message(F.text == "🏨 Найти отель")
-async def hotel(message: Message):
-    await message.answer("Вот отели 👇\nhttps://travel.yandex.ru")
-
-async def main():
-    await dp.start_polling(bot)
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(f"Ты написал: {message.text}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
-if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
